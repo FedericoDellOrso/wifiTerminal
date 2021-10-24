0027d51184804a1f5048201524d4dd02c3f9ebe5
forsus <orsofede@gmail.com>
2021-10-24 22:30:45 +0200

    remove more

--- wifi_python.py
+++ wifi_python.py
@@ -2,6 +2,15 @@ import os
 
 def set_wifi_ssid_psk(ssid, psk):
 	os.system('sudo wpa_cli -i wlan0 remove_network 0')
+	os.system('sudo wpa_cli -i wlan0 remove_network 1')
+	os.system('sudo wpa_cli -i wlan0 remove_network 2')
+	os.system('sudo wpa_cli -i wlan0 remove_network 3')
+	os.system('sudo wpa_cli -i wlan0 remove_network 4')
+	os.system('sudo wpa_cli -i wlan0 remove_network 5')
+	os.system('sudo wpa_cli -i wlan0 remove_network 6')
+	os.system('sudo wpa_cli -i wlan0 remove_network 7')
+	os.system('sudo wpa_cli -i wlan0 remove_network 8')
+	os.system('sudo wpa_cli -i wlan0 remove_network 9')
 	os.system('sudo wpa_cli -i wlan0 add_network')
 	os.system('sudo wpa_cli -i wlan0 set_network 0 ssid ' + '\'"' + ssid + '"\'')
 	os.system('sudo wpa_cli -i wlan0 set_network 0 psk ' + '\'"' + psk + '"\'')
